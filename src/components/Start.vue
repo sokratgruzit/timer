f1e2fe30d1241b78187f03955b5982887bd77ee3
sergo93@gmail.com <sergo93@gmail.com>
2021-02-05 22:09:05 +0400

    test video1

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -56,7 +56,7 @@
         </div>
       </div>
       <div class="right">
-        <video muted loop autoplay playsinline preload="yes" class="start-container__video" ref="video">
+        <video controls="false" muted loop autoplay playsinline preload="yes" class="start-container__video" ref="video">
           <source :src="require(`@/assets/img/start.mp4`)" type="video/mp4">
         </video>
         <img :src="require(`@/assets/img/gradStart.png`)" alt="" class="start-container__gradient">
